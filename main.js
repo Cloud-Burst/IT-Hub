c26fdb06c5813b9bc2a582c0478457a5286a5375
Cloud-Burst <115977282+Cloud-Burst@users.noreply.github.com>
2023-03-30 18:32:48 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+const imgs = document.querySelectorAll("#img-container");
+let currentImg = 0
+imgs[currentImg].classList.add("slide");
+
+// Image function
+const nextImage = () => {
+  imgs[currentImg].classList.remove("slide");
+  currentImg++;
+  if (currentImg >= imgs.length) {
+    currentImg = 0;
+  }
+  imgs[currentImg].classList.add("slide");
+};
+
+setInterval(nextImage, 5000);
