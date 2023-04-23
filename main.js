cb620f2dc2978cc037a0f6d9b4ea9a3c1dd1c1ad
Cloud <Cloud>
2023-04-23 10:47:11 +0800

    removed db

--- main.js
+++ main.js
@@ -15,15 +15,3 @@ const nextImage = () => {
 setInterval(nextImage, 5000);
 
 import { addHoverEffect } from './utils.js';
-import { initializeApp } from "https://wwww.gstatic.com/firebasejs/9.15.0/firebase-app-js";
-import { getDatabase, ref, push} from "https://proposal-db-default-rtdb.asia-southeast1.firebasedatabase.app/";
-
-const appSetting = {
-  databaseURL: "https://proposal-db-default-rtdb.asia-southeast1.firebasedatabase.app/"
-}
-
-const app = initializeApp(appSetting);
-const database = getDatabase(app);
-const cardDB = ref(database, "progList");
-
-console.log(cardDB);
