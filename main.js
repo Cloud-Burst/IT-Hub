05489a5eae3f2c81453d306115a845eca10bbcf6
Cloud <Cloud>
2023-04-27 01:13:36 +0800

    adjusted footer

--- main.js
+++ main.js
@@ -17,6 +17,29 @@ setInterval(nextImage, 5000);
 import { addHoverEffect } from './utils.js';
 
 
+// Get the form element
+const form = document.querySelector("form");
+
+// Add a submit event listener to the form element
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  // Get the values of the input fields
+  const name = document.querySelector("#name").value;
+  const email = document.querySelector("#email").value;
+  const message = document.querySelector("#message").value;
+
+  // Check if any of the fields are empty
+  if (name === "" || email === "" || message === "") {
+    // Show an alert message if any of the fields are empty
+    alert("All fields are required.");
+  } else {
+    // Show a thank you message and reset the form
+    alert(`Thank you! ${name} We have received your message.`);
+    form.reset();
+  }
+});
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
